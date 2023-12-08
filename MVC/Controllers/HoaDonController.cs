31e0170dc0f33487a9288038ac774ebecaab6d39
DaoTrongHieuk2 <hieubeo6944@gmail.com>
2023-12-08 21:32:23 +0700

    excecl HoaDon

--- MVC/Controllers/HoaDonController.cs
+++ MVC/Controllers/HoaDonController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models;
 using OfficeOpenXml;
-
+using MVC.Models.Process;
 namespace MVC.Controllers
 {
     public class HoaDonController : Controller
@@ -19,7 +19,7 @@ namespace MVC.Controllers
         {
             _context = context;
         }
-
+        private ExcelProcess _excelPro = new ExcelProcess();
         // GET: HoaDon
         public async Task<IActionResult> Index()
         {
@@ -179,6 +179,53 @@ namespace MVC.Controllers
         }
 
 
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to server
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    if (file.Length > 0)
+                    {
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            //save file to server
+                            await file.CopyToAsync(stream);
+                            //read data from file and write to database
+                            var dt = _excelPro.ExcelToDataTable(fileLocation);
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                var ps = new HoaDon();
+                                ps.IdHD = dt.Rows[i][0].ToString();
+
+                                ps.IdKH = dt.Rows[i][1].ToString();
+
+                                ps.IdNV = dt.Rows[i][2].ToString();
+                                ps.IdSP = dt.Rows[i][3].ToString();
+                                ps.MyProperty = Convert.ToInt32(dt.Rows[i][4].ToString());
+                                _context.Add(ps);
+                            }
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+            }
+            return View();
+        }
 
 
 
