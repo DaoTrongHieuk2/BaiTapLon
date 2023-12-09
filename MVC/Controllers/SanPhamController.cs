fc9399508d4befa07f41f3738ee1320b4e344d8f
TrungHieu202 <dongtrunghieu2002@gmail.com>
2023-12-09 14:25:11 +0700

    excel san pham

--- MVC/Controllers/SanPhamController.cs
+++ MVC/Controllers/SanPhamController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models;
-
+using MVC.Models.Process;
 namespace MVC.Controllers
 {
     public class SanPhamController : Controller
@@ -18,7 +18,7 @@ namespace MVC.Controllers
         {
             _context = context;
         }
-
+        private ExcelProcess _excelPro = new ExcelProcess();
         // GET: SanPham
         public async Task<IActionResult> Index()
         {
@@ -158,6 +158,50 @@ namespace MVC.Controllers
         private bool SanPhamExists(string id)
         {
           return (_context.SanPham?.Any(e => e.IdSP == id)).GetValueOrDefault();
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+                {
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    {
+                        ModelState.AddModelError("", "Please choose excel file to upload!");
+                    }
+                    else
+                    {
+                        //rename file when upload to server
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                        var fileLocation = new FileInfo(filePath).ToString();
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                  //read data from file and write to database
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                for(int i = 0; i < dt.Rows.Count; i++)
+                                {
+                                    var ps = new SanPham();
+                                    ps.IdSP = dt.Rows[i][0].ToString();
+                                    ps.NameSP = dt.Rows[i][1].ToString();
+                                    ps.NumberSP = dt.Rows[i][2].ToString();
+                                    ps.PriceSP = dt.Rows[i][3].ToString();
+                                    _context.Add(ps);
+                                }
+                                await _context.SaveChangesAsync();
+                                return RedirectToAction(nameof(Index));
+                            }                       
+                         }
+                    }
+                 }
+           return View();
         }
     }
 }
